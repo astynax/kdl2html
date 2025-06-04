06842cc7c4869b8c407ae727e5e59ecc3b1cbac1
astynax <astynax@users.noreply.github.com>
2025-06-04 09:05:59 +0400

    update some logic

--- kdl2html.py
+++ kdl2html.py
@@ -1,32 +1,47 @@
-#!/usr/bin/env -S uv run --no-project --quiet
-# /// script
-# requires-python = ">=3.12"
-# dependencies = [
-#     "cuddle==1.0.6",
-#     "htpy==24.10.1",
-# ]
-# ///
-
 import sys
-from itertools import chain
 
 import htpy
-from markupsafe import Markup
+from markupsafe import escape, Markup
 import cuddle
 
 
-def htmlize(node: cuddle.Node | cuddle.Document) -> str:
-    if isinstance(node, cuddle.Document):
-        return ''.join(str(htmlize(n)) for n in node.nodes)
-    el = getattr(htpy, node.name)(**node.properties)
-    if not isinstance(el, htpy.VoidElement):
-        el = el[list(chain(
-            (Markup(str(a)) for a in node.arguments),
-            map(htmlize, node.children),
-        ))]
-    elif node.arguments or node.children:
-        raise TypeError("Void node", node.name, "cannot have children")
-    return el
+def _htmlize(node: cuddle.Node) -> htpy.Fragment:
+    if node.name == "_":
+        if node.children or node.properties:
+            raise ValueError(
+                f"Text nodes cannot have children and/or properties: {node}",
+            )
+        return htpy.fragment[(escape(arg) for arg in node.arguments)]
+    try:
+        tag: htpy.Element | htpy.VoidElement
+        tag = getattr(htpy, node.name)
+    except AttributeError:
+        raise ValueError(f"Unknown tag: {node.name}")
+    else:
+        el = tag(**node.properties)
+        match el:
+            case htpy.VoidElement() if node.arguments or node.children:
+                raise TypeError(f"Void node {node.name} cannot have children")
+            case htpy.Element():
+                if tag is htpy.script or tag is htpy.style:
+                    if len(node.arguments) > 1 or node.children:
+                        raise ValueError("Script and style tags should have"
+                                         " exatcly one (raw) string argument")
+                    args = (
+                        (Markup(arg) for arg in node.arguments),
+                    )
+                else:
+                    args = (
+                        (htpy.fragment[escape(arg)] for arg in node.arguments),
+                        map(_htmlize, node.children),
+                    )
+                return htpy.fragment[el[*args]]
+            case _:
+                return htpy.fragment[el]
+
+
+def htmlize(doc: cuddle.Document) -> str:
+    return str(htpy.fragment[map(_htmlize, doc.nodes)])
 
 
 def main():
